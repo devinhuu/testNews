d7f5b60d33c37e49628f94d6dacc4d1826325c50
devinhu <devinhu@163.com>
2013-11-17 14:14:23 +0800

    Revert "test"
    This reverts commit 12985c2c81e3724ad1c0dfe8991a167d9377c5d9.

--- test/src/test/interfaceA.java
+++
@@ -1,6 +0,0 @@
-package test;
-
-public interface interfaceA {
-	public void aa();
-	public void BB();
-}
--- test/src/test/interfaceB.java
+++
@@ -1,5 +0,0 @@
-package test;
-
-public interface interfaceB extends interfaceA {
-	public void aa();
-}
